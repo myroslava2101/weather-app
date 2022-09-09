c86cf3089d93511b3f6548d15d26f72c8ada8a40
myroslava2101 <111093419+myroslava2101@users.noreply.github.com>
2022-09-10 01:24:03 +0300

    Added the js templates

--- src/app.js
+++ src/app.js
@@ -27,6 +27,39 @@ function formatDate(timestamp) {
     axios.get(apiUrl).then(displayForecast);
   }
   
+  function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+  
+    let days = ["Thu", "Fri", "Sat", "Sun"];
+  
+    let forecastHTML = `<div class="row">`;
+    days.forEach(function (day) {
+      forecastHTML =
+        forecastHTML +
+        `
+        <div class="col-2">
+          <div class="weather-forecast-date">${day}</div>
+          <img
+            src="http://openweathermap.org/img/wn/50d@2x.png"
+            alt=""
+            width="42"
+          />
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max"> 18° </span>
+            <span class="weather-forecast-temperature-min"> 12° </span>
+          </div>
+        </div>
+    `;
+    });
+  
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+  }
+  function getForecast(coordinates) {
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+    
 function displayTemperature (response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
@@ -91,4 +124,4 @@ function handleSubmit(event) {
   form.addEventListener("submit", handleSubmit);
   
   search("New York");
-
+displayForecast ();
